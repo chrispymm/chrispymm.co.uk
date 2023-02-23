459a2ff25d962b236d29795ef36c1234edaa79f6
Chris Pymm <chris@chrispymm.co.uk>
2023-02-23 23:41:11 +0000

    add drafts feature

--- .eleventy.js
+++ .eleventy.js
@@ -34,7 +34,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addNunjucksFilter("limit", function(array, limit) {
     return array.slice(0, limit);
   });
-  
+
   // date filter
   eleventyConfig.addNunjucksFilter("date", function(date, format) {
     return moment(date).format(format);
@@ -50,6 +50,36 @@ module.exports = function(eleventyConfig) {
     ],
   });
 
+  // When `permalink` is false, the file is not written to disk
+	eleventyConfig.addGlobalData("eleventyComputed.permalink", function() {
+		return (data) => {
+			// Always skip during non-watch/serve builds
+			if(data.draft && !process.env.BUILD_DRAFTS) {
+				return false;
+			}
+
+			return data.permalink;
+		}
+	});
+
+  // When `eleventyExcludeFromCollections` is true, the file is not included in any collections
+	eleventyConfig.addGlobalData("eleventyComputed.eleventyExcludeFromCollections", function() {
+		return (data) => {
+			// Always exclude from non-watch/serve builds
+			if(data.draft && !process.env.BUILD_DRAFTS) {
+				return true;
+			}
+
+			return data.eleventyExcludeFromCollections;
+		}
+	});
+
+	eleventyConfig.on("eleventy.before", ({runMode}) => {
+		// Set the environment variable
+		if(runMode === "serve" || runMode === "watch") {
+			process.env.BUILD_DRAFTS = true;
+		}
+	});
 };
 
 /**
---
+++ posts/how-to-build-create-post-page-using-serverless-functions.md
@@ -0,0 +1,12 @@
+---
+draft: true
+---
+register github oauth app for dev and prod
+
+
+Add client id and secret to .env file
+
+While in the .env file add GITHUB REPO wiht name of repo to add content to
+
+Add AUTHORIZED_USERS key with comma separated string of github usernames that
+should be allowed to login
