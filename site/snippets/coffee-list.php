bf55484ee326d29db26211b75e06eaea5f884e7a
Chris Pymm <chris@chrispymm.co.uk>
2024-05-31 11:13:15 +0100

    tidy coffee list output

--- site/snippets/coffee-list.php
+++ site/snippets/coffee-list.php
@@ -1,6 +1,9 @@
 <article class="flow" style="--flow-space:0.6rem;">
     <h2 class="font-size-0"><a href="<?=$coffee->url()?>"><?= $coffee->title() ?></a></h2>
-    <p class="font-size--1"><?= $coffee->origin() ?> | <?= $coffee->process() ?> | <?= $coffee->cultivar() ?></p>
+    <p class="font-size--1">
+        <?= $coffee->origin() ?: '' ?>
+        <?= $coffee->process() != '' ? ' | '.$coffee->process() : '' ?>
+        <?= $coffee->cultivar()!= '' ? ' | '.$coffee->cultivar() : '' ?></p>
     <?php if( $rating ): ?>
         <?php snippet('rating', ['rating' => (float) $coffee->rating()->value()]) ?>
     <?php endif ?>
